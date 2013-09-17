4c590d442df313173aab876f608fb931baabb104
Matt Dumford <mdumford99@gmail.com>
2013-09-16 22:59:42 -0500

    finished part 4

--- statsh.c
+++ statsh.c
@@ -13,7 +13,7 @@
 #include <sys/resource.h>
 
 int main(int argc, char **argv, char** envp){
-	printf("Matt Dumford - mdumfo2\n\n");
+	printf("Matt Dumford - mdumfo2\n");
 	
 	char *inputHistory[100];
 	struct rusage rusageHistory[100];
@@ -24,8 +24,7 @@ int main(int argc, char **argv, char** envp){
 		size_t inSize = 100;
 		int numChars;
 		
-		in = (char *) malloc(inSize + 1);
-		printf("> ");
+		printf("\n> ");
 
 		//get input
 		numChars = getline(&in, &inSize, stdin);
@@ -63,60 +62,65 @@ int main(int argc, char **argv, char** envp){
 				}
 			}
 			else{ //tokenize and run exec
-			//	int numCommands = 0;
-			//	char *commands[50];
-			//	char *command = strtok(in, "|");
+				int numCommands = 0;
+				char *commands[50];
+				char *command = strtok(in, "|");
 
-			//	while(command != NULL){
-					
-				//}
-				
-
-				//tokenize input
-				int numToks = 0;
-				char *input[50];
-				char *tok = strtok(in, " ");
-				
-				while(tok != NULL){
-					input[numToks] = tok;
-					tok = strtok(NULL, " ");
-	
-					numToks++;
-				} 
-				
-				//end the array with NULL	
-				input[numToks] = NULL;
-				
-				pid_t pid = fork();
-
-				if(pid < 0){
-					printf("Error forking!");
-					exit(-1);
+				while(command != NULL){
+					commands[numCommands] = command;
+					command = strtok(NULL, "|");
+					numCommands++;
 				}
-				else if(pid == 0){ //child process
-					execvp(*input, input);
+
+				commands[numCommands] = NULL;
+				
+				int j;				
+				for(j=0; j<numCommands; j++){
 	
-					//if exec returns and this runs, something broke.
-					printf("Command not found: %s\n", input[0]);
-					exit(-1);
-				}
-				else{ //parent process
-					struct rusage rusage;
-					int status;
+					//tokenize input
+					int numToks = 0;
+					char *input[50];
+					char *tok = strtok(commands[j], " ");
 					
-					pid_t pid2 = wait4(pid, &status, 0, &rusage);
+					while(tok != NULL){
+						input[numToks] = tok;
+						tok = strtok(NULL, " ");
+						numToks++;
+					} 
 					
-					if(pid2 != -1 && WIFEXITED(status) && !WEXITSTATUS(status)){
-						inputHistory[i] = (char *) malloc(strlen(in));
-						strcpy(inputHistory[i], in);
-						rusageHistory[i] = rusage;
-						i++;
+					//end the array with NULL	
+					input[numToks] = NULL;
+					
+					pid_t pid = fork();
+	
+					if(pid < 0){
+						printf("Error forking!");
+						exit(-1);
+					}
+					else if(pid == 0){ //child process
+						execvp(*input, input);
+		
+						//if exec returns and this runs, something broke.
+						printf("Command not found: %s\n", input[0]);
+						exit(-1);
+					}
+					else{ //parent process
+						struct rusage rusage;
+						int status;
 						
-						printf("\nUser time: %lu.%06lu (s)\n", rusage.ru_utime.tv_sec, rusage.ru_utime.tv_usec);
-						printf("System time: %lu.%06lu (s)\n", rusage.ru_stime.tv_sec, rusage.ru_stime.tv_usec);
+						pid_t pid2 = wait4(pid, &status, 0, &rusage);
+						
+						if(pid2 != -1 && WIFEXITED(status) && !WEXITSTATUS(status)){
+							inputHistory[i] = (char *) malloc(strlen(commands[j]));
+							strcpy(inputHistory[i], commands[j]);
+							rusageHistory[i] = rusage;
+							i++;
+							
+							printf("\tUser time: %lu.%06lu (s)\n", rusage.ru_utime.tv_sec, rusage.ru_utime.tv_usec);
+							printf("\tSystem time: %lu.%06lu (s)\n\n", rusage.ru_stime.tv_sec, rusage.ru_stime.tv_usec);
+						}
 					}
 				}
-				free(in);
 			}
 		}
 	}
